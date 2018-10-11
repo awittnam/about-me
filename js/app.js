b0a73f752e104441e471e3423294ea8917f889f1
Harris <harris.richardson87@gmail.com>
2018-10-10 22:39:30 -0700

    Both Questions

--- js/app.js
+++ js/app.js
@@ -34,3 +34,49 @@ console.log('user is a friend'); }
 else{alert('some friend you are.');
 console.log('user is not friend');
 };
+
+
+alert("Now lets play a game, guess how many countries I have been to in my life, you only have 4 chances to get this right so we need to focus")
+var correctAnswer = 6;
+var guess = prompt("Guess a number between 1-10");
+var turn = 5;
+
+for(var i=1; i< 4; i++){
+  if( guess == correctAnswer){
+    alert("You have chosen wisely. I have been to Argentina, Ireland, France, Spain, Italy & Mexico");
+    break;
+
+  } else{
+    
+    if( guess > correctAnswer){alert("Too high guess again")};
+    if( guess < correctAnswer){alert("Too low guess again")};
+    
+    var guess = prompt("Guess again, you have made " + i + " attempts!");
+  }
+
+
+}
+ if( guess != correctAnswer){alert("I'm sorry you have failed please leave my website");
+ }
+
+alert("Now let's guess which cities i have lived in, you have seven tries to complete it")
+var city = ["Barcelona","Dublin","London"];
+var guess = prompt("Guess a city");
+var i = 0;
+var attempts = 7;
+while(i < 7)
+if (guess === city[1] || guess === city[0] || guess === city[2] ){
+  i++;
+  attempts --;
+  alert('you got it, it only took you' + i + ' attempts out of seven!');
+  break;
+} else {
+  i++;
+  attempts --;
+  alert ("WRONG you have " + attempts + " more tries to get it right")
+  var guess = prompt("Guess a city");
+  if (attempts === 0){
+    alert("Leave in shame.")
+    break;
+  }
+}
